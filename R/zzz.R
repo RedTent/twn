19cdec33c7291f2b24fb62cb279fb600492bf377
Johan van Tent <tentvanjohan@hotmail.com>
2020-02-22 13:28:47 +0100

    kleur aangepast om ook voor een licht scherm te werken.

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste(crayon::green("twn"), 
+  packageStartupMessage(paste(("twn"), 
                               "gebruikt de", 
-                              crayon::green("TWN-lijst"), 
+                              ("TWN-lijst"), 
                               "van", 
-                              crayon::white(attr(twn_lijst, "datum_twn_lijst"))))
+                              crayon::red(attr(twn_lijst, "datum_twn_lijst"))))
 }
