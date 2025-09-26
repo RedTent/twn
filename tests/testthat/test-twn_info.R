444ba170667c4006f495ea8ec8c0584a6cb82b8a
Johan van Tent <tentvanjohan@hotmail.com>
2025-09-26 14:53:45 +0200

    tests voor aliases en twn_datum()

--- tests/testthat/test-twn_info.R
+++ tests/testthat/test-twn_info.R
@@ -28,6 +28,16 @@ test_that("twn_info functies werken", {
   expect_equal(twn_status(namen, code = FALSE), c("Synonym", "Preferred name", "Preferred name", NA, NA))
   expect_equal(as.character(twn_taxonlevel(namen)), c("Species", "Genus", "Species", NA, NA))
   expect_equal(twn_taxontype(c("Bufo", "Plantae")), c("Amphibia", "Macroalgae Macrophytes Phytoplankton"))
+  # NB Test fails als er een wijziging in de datum is
+  expect_equal(twn_datum(namen), as.Date(c("2019-01-22", "2008-11-25", "2008-11-25", NA, NA )))
+  
+  
+  # testen aliassen
+  expect_equal(twn_refername(namen), c("Epidalea calamita", "Bufo", "Bufo bufo", NA, NA))
+  expect_equal(twn_ouder(namen), c("Epidalea", "Bufonidae", "Bufo", NA, NA))
+  expect_equal(twn_nednaam(namen), c("Rugstreeppad", NA, "Gewone pad", NA, NA))
+  expect_equal(twn_date(namen), as.Date(c("2019-01-22", "2008-11-25", "2008-11-25", NA, NA )))
+  expect_equal(as.character(twn_taxonniveau(namen)), c("Species", "Genus", "Species", NA, NA))
   
   expect_is(twn_taxonlevel(namen), "factor")
 })
